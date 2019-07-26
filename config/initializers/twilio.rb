e46142fefe62a72fdfa4060a674dda6f0b711dcb
drbarq <J.Tustin@gmail.com>
2019-07-26 12:25:49 -0600

    changed env

--- config/initializers/twilio.rb
+++ config/initializers/twilio.rb
@@ -1,6 +1,6 @@
 Twilio.configure do |config|
-    # config.account_sid = Rails.application.credentials.twilio_account_sid
-    # config.account_sid = Rails.application.credentials.twilio_auth_token
-    config.account_sid = ENV['twilio_Account_sid']
-    config.auth_token = ENV['twilio_auth_token']
+    config.account_sid = Rails.application.credentials.twilio_account_sid
+    config.account_sid = Rails.application.credentials.twilio_auth_token
+    # config.account_sid = ENV['twilio_Account_sid']
+    # config.auth_token = ENV['twilio_auth_token']
   end
